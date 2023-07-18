4d8485f3ee4241e90a468becb7ec77adc6bc154f
MUNAZIR <60435223+Munazirraza@users.noreply.github.com>
2023-07-18 20:20:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+let timer = document.getElementById('timer');
+let newDate =new Date();
+let date = new Date().toLocaleString();
+
+let fullDate = `${newDate.getDate()}/${newDate.getMonth()}/${newDate.getFullYear()}, `
+// let year = newDate.getFullYear()
+timer.innerText = date
